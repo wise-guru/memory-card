d7b447d9de96561629c2e993eed599cfc15a73a5
wise-guru <wise.guru123@gmail.com>
2022-08-21 19:41:54 -0400

    format

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Cards = (props) => {
   return (
     <div className="card" onClick={() => props.incrementScore(props.name)}>
-      <img className="cardImage" src={props.image} alt={props.name} />
       <div className="cardName">{props.name}</div>
+      <img className="cardImage" src={props.image} alt={props.name} />
     </div>
   );
 };
